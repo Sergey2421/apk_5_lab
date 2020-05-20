8483c3b34fd5afffa521aa07d19c0140cacebee8
Sergey2421 <61668850+Sergey2421@users.noreply.github.com>
2020-05-20 20:24:47 +0300

    Update apk5.cpp

--- apk5.cpp
+++ apk5.cpp
@@ -1,86 +1,193 @@
-#include <io.h>
-#include <dos.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <conio.h>
+#include <dos.h>
+
+unsigned int delayTime = 0;
 
 char date[6];
-unsigned int delayTime = 0;
-const unsigned int registers[] = { 0x00, 0x02, 0x04, 0x07, 0x08, 0x09 };
 
-void interrupt(*lastTime)(...);
-void interrupt(*lastAlarm) (...);
+unsigned int registers[] = { 0x00, 0x02, 0x04, 0x07, 0x08, 0x09 };
+
+void interrupt(*oldTimer)(...);
+void interrupt(*oldAlarm) (...);
+
+void getTime();
+void freeze(void);
+void unfreeze(void);
+void wait(void);
+void setTimeManually();
+void delay(unsigned int, int);
+void setAlarm();
 void resetAlarm();
-int getValueBetweenAsBCD(int min, int max);
-void enterTime();
-int BSDtoDecimal(int BCD);
+void inputTime();
+int bcdToDec(int);
+int decToBcd(int);
 
-void interrupt newTime(...) // новый обработчик прерываний часов
+void interrupt newTimer(...)
 {
 	delayTime++;
-	outp(0x70, 0xC);
+	outp(0x70, 0x0C);
 	inp(0x71);
 
 	outp(0x20, 0x20);
 	outp(0xA0, 0x20);
 }
 
-void interrupt newAlarm(...) // новый обработчик прерываний будильника
+void interrupt newAlarm(...)
 {
-	puts("ALARM IS RINGING........");
-	lastAlarm();
+	puts("Alarm");
+	oldAlarm();
 	resetAlarm();
+}
+
+int main()
+{
+	clrscr();
+	int delayMillisecond;
+	while (1) {
+		printf("1 - Current time\n");
+		printf("2 - Set time\n");
+		printf("3 - Set alarm\n");
+		printf("4 - Set delay\n");
+		printf("0 - Exit\n");
+		switch (getch()) {
+		case '1':
+			getTime();
+			break;
+
+		case '2':
+			setTimeManually();
+			break;
+
+		case '3':
+			setAlarm();
+			break;
+		case '4':
+			fflush(stdin);
+			printf("Input delay in millisecond: ");
+			scanf("%d", &delayMillisecond);
+			delay(delayMillisecond, 1);
+			break;
+		case '0':
+			return 0;
+			break;
+		default:
+			clrscr();
+			break;
+		}
+	}
 }
 
 void getTime()
 {
-	for (int i = 0; i < 6; i++)
+
+	char* monthToText[] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
+	clrscr();
+	int i = 0;
+	for (i = 0; i < 6; i++)
 	{
 		outp(0x70, registers[i]);
 		date[i] = inp(0x71);
 	}
 
-	int decTime[6];
-	for (int j = 0; j < 6; j++)
-		decTime[j] = BSDtoDecimal(date[j]);
-
-
-	printf("%2d:%2d:%2d\n", decTime[2], decTime[1], decTime[0]); // час, минута, секунда
-	printf("%2d %s 20%2d\n", decTime[3], decTime[4], decTime[5]); // день, месяц, год
-}
-
-void setTime()
-{
-	enterTime();
-	disable();
-
-	do
+	int decDate[6];
+	for (i = 0; i < 6; i++)
 	{
-		outp(0x70, 0xA); // выбор регистра А
-	} while (inp(0x71) & 0x80); // Если 7й бит установлен в 0, то это значит, что данные готовы для чтения
-
-	//Остановка счётчика RTC
-	outp(0x70, 0xB);
-	outp(0x71, inp(0x71) | 0x80); // Если 7й бит установлен в 1, то счётчик остановлен
-
-	for (int i = 0; i < 6; i++)
-	{
-		outp(0x70, registers[i]);
-		outp(0x71, date[i]);
+		decDate[i] = bcdToDec(date[i]);
 	}
 
-	outp(0x70, 0xB);
-	outp(0x71, inp(0x71) & 0x7F); // Если 7й бит установлен в 0, то счётчик запущен
-
-	enable();
-	system("cls");
+	printf(" %2d:%2d:%2d", decDate[2], decDate[1], decDate[0]);
+	printf(" %s, %2d, 20%2d\n", monthToText[decDate[4] - 1], decDate[3], decDate[5]);
 }
 
-void delay(unsigned int ms)
+void setTimeManually(void) {
+	int value;
+	freeze();                            // disable rtc update
+
+	do {
+		printf("Enter hour: ");
+		fflush(stdin);
+		scanf("%d", &value);
+	} while (value > 23 || value < 0);
+	outp(0x70, 0x04);                    // Register of current hour
+	outp(0x71, decToBcd(value));        // Write value
+
+	do {
+		printf("Enter minute: ");
+		fflush(stdin);
+		scanf("%d", &value);
+	} while (value > 59 || value < 0);
+	outp(0x70, 0x02);                    // Register of current minute
+	outp(0x71, decToBcd(value));
+
+	do {
+		printf("Enter second: ");
+		fflush(stdin);
+		scanf("%d", &value);
+	} while (value > 59 || value < 0);
+	outp(0x70, 0x00);                    // Register of current second
+	outp(0x71, decToBcd(value));
+
+	do {
+		printf("Enter number day of month: ");
+		fflush(stdin);
+		scanf("%d", &value);
+	} while (value > 31 || value < 1);
+	outp(0x70, 0x07);                    // Register of current day of month
+	outp(0x71, decToBcd(value));
+
+	do {
+		printf("Enter mounth: ");
+		fflush(stdin);
+		scanf("%d", &value);
+	} while (value > 12 || value < 1);
+	outp(0x70, 0x08);                    // Register of current month
+	outp(0x71, decToBcd(value));
+
+	do {
+		printf("Enter year: ");
+		fflush(stdin);
+		scanf("%d", &value);
+	} while (value > 99 || value < 0);
+	outp(0x70, 0x09);                    // Register of current year(last two numbers)
+	outp(0x71, decToBcd(value));
+
+	unfreeze();                            // enable rtc update
+}
+
+void freeze(void) {                        // Rtc update disabling
+	unsigned char value;
+	wait();                                // until rtc are busy
+	outp(0x70, 0x0B);
+	value = inp(0x71);                    // get B register
+	value |= 0x80;                        // change bit 7 to 1 to disable rtc update (disables the counter)
+	outp(0x71, value);                    // write new B register value
+}
+
+void unfreeze(void) {                    // rtc update enabling
+	unsigned char value;
+	wait();                                // until rtc are busy
+	outp(0x70, 0x0B);
+	value = inp(0x71);                    // get B register
+	value -= 0x80;                        // change bit 7 to 0 to allow rtc update (enable the counter)
+	outp(0x70, 0x0B);
+	outp(0x71, value);                    // write new B register value
+}
+
+void wait(void) {
+	do {
+		outp(0x70, 0x0A);               // Time setting
+	} while (inp(0x71) & 0x80);            // 0x80 = 10000000 while 7-th bit - 1, rtc is busy
+}
+
+void delay(unsigned int ms, int printable)
 {
 	disable();
-	lastTime = getvect(0x70);
-	setvect(0x70, newTime);
+
+	oldTimer = getvect(0x70);
+	setvect(0x70, newTimer);
+
 	enable();
 
 	outp(0xA1, inp(0xA1) & 0xFE);
@@ -90,152 +197,112 @@ void delay(unsigned int ms)
 
 	delayTime = 0;
 	while (delayTime <= ms);
-	puts("delay is over");
-	setvect(0x70, lastTime);
+	if (printable)
+		puts("Delay's end");
+	setvect(0x70, oldTimer);
+	return;
 }
 
 void setAlarm()
 {
-	enterTime();
+	inputTime();
+
 	disable();
 
-	do {
+	unsigned int res;
+	do
+	{
 		outp(0x70, 0xA);
-	} while (inp(0x71) & 0x80); // Если 7й бит установлен в 0, то это значит, что данные готовы для чтения
+		res = inp(0x71) & 0x80;
+	} while (res);
 
-	// установка часов в регистр будильника
 	outp(0x70, 0x05);
 	outp(0x71, date[2]);
 
-	// установка минут в регистр будильника
 	outp(0x70, 0x03);
 	outp(0x71, date[1]);
 
-	// установка секунд в регистр будильника
 	outp(0x70, 0x01);
 	outp(0x71, date[0]);
 
-	outp(0x70, 0xB); // выбор регистра B
-	outp(0x71, (inp(0x71) | 0x20)); //Если 5й бит установлен в 1, то разрешаем прерывания будильника
+	outp(0x70, 0xB);
 
-	// переопределение прерывания будильника
-	lastAlarm = getvect(0x4A);
+	outp(0x71, (inp(0x71) | 0x20));
+
+	oldAlarm = getvect(0x4A);
 	setvect(0x4A, newAlarm);
-	outp(0xA1, (inp(0xA0) & 0xFE)); // Если 0й бит в 0, то разрешаем прерывания от RTC
+	outp(0xA1, (inp(0xA0) & 0xFE));
 
 	enable();
-	puts("alarm is enabled");
+	printf("Alarm enabled\n");
 }
 
 void resetAlarm()
 {
-	if (lastAlarm == NULL)
+	if (oldAlarm == NULL)
+	{
 		return;
+	}
 
 	disable();
 
-	// возврат старого прерывания
-	setvect(0x4A, lastAlarm);
+	setvect(0x4A, oldAlarm);
 	outp(0xA1, (inp(0xA0) | 0x01));
 
+	unsigned int res;
 	do
 	{
 		outp(0x70, 0xA);
-	} while (inp(0x71) & 0x80); // Если 7й бит установлен в 0, то это значит, что данные готовы для чтения
+		res = inp(0x71) & 0x80;
+	} while (res);
 
-	outp(0x70, 0x05); // 0x05 - часы
+	outp(0x70, 0x05);
 	outp(0x71, 0x00);
 
-	outp(0x70, 0x03); // 0x03 - минуты
+	outp(0x70, 0x03);
 	outp(0x71, 0x00);
 
-	outp(0x70, 0x01); // 0x01 - секунды
+	outp(0x70, 0x01);
 	outp(0x71, 0x00);
 
 	outp(0x70, 0xB);
-	outp(0x71, (inp(0x71) & 0xDF)); //Если 5й бит установлен в 0, то запрещаем прерывания будильника
+	outp(0x71, (inp(0x71) & 0xDF));
 
 	enable();
 }
 
-int getValueBetweenAsBCD(int min, int max)
+void inputTime()
 {
-	int value;
-	do
-	{
-		rewind(stdin);
-		scanf("%d", &value);
-	} while (value > max || value < min);
+	int n;
 
-	return value / 10 * 16 + value % 10;
+	do {
+		fflush(stdin);
+		printf("Input hours: ");
+		scanf("%i", &n);
+	} while ((n > 23 || n < 0));
+	date[2] = decToBcd(n);
+
+	do {
+		fflush(stdin);
+		printf("Input minutes: ");
+		scanf("%i", &n);
+	} while (n > 59 || n < 0);
+	date[1] = decToBcd(n);
+
+	do {
+		fflush(stdin);
+		printf("Input seconds: ");
+		scanf("%i", &n);
+	} while (n > 59 || n < 0);
+	date[0] = decToBcd(n);
 }
 
-void enterTime()
+int bcdToDec(int bcd)
 {
-	printf("\nset year: ");
-	date[5] = getValueBetweenAsBCD(0, 2100);
-	printf("\nset month: ");
-	date[4] = getValueBetweenAsBCD(1, 12);
-	printf("\nset day: ");
-	date[3] = getValueBetweenAsBCD(1, 365);
-	printf("\nset hours: ");
-	date[2] = getValueBetweenAsBCD(0, 23);
-	printf("\nset minutes: ");
-	date[1] = getValueBetweenAsBCD(0, 59);
-	printf("\nset seconds: ");
-	date[0] = getValueBetweenAsBCD(0, 59);
+	return ((bcd / 16 * 10) + (bcd % 16));
 }
 
-int BSDtoDecimal(int BCD)
+int decToBcd(int dec)
 {
-	return ((BCD / 16 * 10) + (BCD % 16));
-}
-
-
-int main()
-{
-	while (1)
-	{
-		printf("1 - show time\n2 - set delay\n3 - create alarm\n4 - set time\n0 - exit\n");
-
-		switch (getch())
-		{
-		case '1':
-		{
-			system("cls");
-			getTime();
-			break;
-		}
-
-		case '2':
-		{
-			system("cls");
-			printf("set time for delay(in ms): ");
-			delay(BSDtoDecimal(getValueBetweenAsBCD(0, 10000)));
-			break;
-		}
-
-		case '3':
-		{
-			system("cls");
-			setAlarm();
-			break;
-		}
-		case '4':
-		{
-			system("cls");
-			setTime();
-			break;
-		}
-		case '0':
-		{
-			return 0;
-		}
-		default:
-		{
-			system("cls");
-			break;
-		}
-		}
-	}
+	return ((dec / 10 * 16) + (dec % 10));
 }
